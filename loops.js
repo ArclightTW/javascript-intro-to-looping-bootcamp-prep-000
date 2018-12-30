1f720fbe088203b3241102959e23c59d9f7d72e5
Tom Wright <tomawright95@gmail.com>
2018-12-30 16:18:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,5 @@
 function forLoop(array) {
   for (var i = 1; i < 25; i++) {
     array.push("I am ${i} strange loop${i === 0 ? '' : 's'}.")
-    console.log("Hello World the " + i + " time");
   }
 }
